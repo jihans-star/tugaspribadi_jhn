3902dc05bf974ae41702053b5a96182d3c0f8167
gishalestari897-ops <gishalestari897@gmail.com>
2025-09-02 07:53:41 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tugaspribadi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugaspribadi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tugaspribadi/widget.dart';
+
+void main() {
+  runApp(const JeApp());
+}
+
+class JeApp extends StatelessWidget {
+  const JeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Aplikasi Flutter",
+      debugShowCheckedModeBanner: false,
+      home: Layoutje(),
+    );
+  }
+}
